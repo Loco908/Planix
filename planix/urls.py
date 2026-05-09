65406b666fa81f00200589b26a7e68d9ca19a8dc
Luis Eduardo <ramireshernandezluiseduardo5@gmail.com>
2026-05-09 11:22:07 -0600

    feat: Add traditional user registration

--- planix/urls.py
+++ planix/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # Rutas de autenticación
     path('', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
     path('social-auth/', include('social_django.urls', namespace='social')),
     
     # Rutas de la app Projects
